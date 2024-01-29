591fa67965aa6cb49f46b33c64f65b1e62a173f9
Sam-Waithaka <sw.waithakasamuel@gmail.com>
2024-01-29 07:23:14 +0300

    fixed error in map and filter

--- functional_programming/map & filter.py
+++ functional_programming/map & filter.py
@@ -4,16 +4,16 @@ def find_coffee(coffee):
     if coffee[0] == 'c':
         return coffee
 
-# map_cofffee = map(find_coffee, menu)
+map_cofffee = map(find_coffee, menu)
 
-# print(map_cofffee)
+print(map_cofffee)
 
-# for x in map_cofffee:
-#     print(x)
+for x in map_cofffee:
+    print("Map",x)
     
 filter_coffee = filter(find_coffee, menu)
 print(filter_coffee)
 
 for x in filter_coffee:
-    print(x)
+    print("Filter",x)
     
